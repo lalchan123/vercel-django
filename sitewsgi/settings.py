c6199d745f67517f131e19f5c305a2549a6e02da
lalchan123 <lalchanbadsa12345@gmail.com>
2024-10-19 10:02:50 -0600

    c

--- sitewsgi/settings.py
+++ sitewsgi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,9 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'sitewsgi.urls'
+
+CORS_ALLOWED_ORIGINS = True
 
 TEMPLATES = [
     {
